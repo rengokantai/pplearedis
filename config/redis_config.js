f78d2098303ceb33ed6a7196682fe60bb987e084
Hernan Y.Ke <yuriqiao@gmail.com>
2015-11-28 22:49:45 -0500

    config file

---
+++ config/redis_config.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Hernan Y.Ke on 2015/11/28.
+ */
+var config ={
+    host:'pub-redis-17674.us-east-1-2.2.ec2.garantiadata.com',
+    port:17674,
+    options:{
+        auth_pass:'123456'
+    },
+    db:1
+};
+
+module.exports = config;
